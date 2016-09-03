a72858807ef707d4211c1d86d4552fd65caf64e7
chris48s <c.mshaw@yahoo.co.uk>
2016-09-03 22:14:55 +0100

    add ckan scraper

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     author="chris48s",
     license="MIT",
     url="https://github.com/wdiv-scrapers/dc-base-scrapers/",
-    py_modules=['arcgis_scraper', 'common', 'geojson_scraper', 'gml_scraper'],
+    py_modules=[
+        'arcgis_scraper',
+        'ckan_scraper',
+        'common',
+        'geojson_scraper',
+        'gml_scraper',
+    ],
 )
