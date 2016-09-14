cc42bd3f2158b5e56d80cc8ae53a1116dfe32fb7
chris48s <c.mshaw@yahoo.co.uk>
2016-09-14 20:12:23 +0100

    install dependencies with setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='dc_base_scrapers',
@@ -6,4 +6,11 @@ setup(
     license="MIT",
     url="https://github.com/wdiv-scrapers/dc-base-scrapers/",
     packages=['dc_base_scrapers'],
+    install_requires = [
+        'lxml==3.6.4',
+        'retry==0.9.2',
+        'scraperwiki==0.5.1',
+        'arcgis2geojson'
+    ],
+    dependency_links = ['git+https://github.com/chris48s/arcgis2geojson.git#egg=arcgis2geojson-0.0.0']
 )
