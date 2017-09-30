9de728ef537cd5251e90b8bbd55128dbbcfaf403
chris48s <chris.shaw480@gmail.com>
2017-09-30 21:27:18 +0100

    allow HashOnlyScraper to sync raw data to GitHub

--- dc_base_scrapers/hashonly_scraper.py
+++ dc_base_scrapers/hashonly_scraper.py
@@ -1,15 +1,17 @@
 from dc_base_scrapers.common import (
     BaseScraper,
-    get_data_from_url
+    get_data_from_url,
+    sync_file_to_github
 )
 
 
 class HashOnlyScraper(BaseScraper):
 
-    def __init__(self, url, council_id, table):
+    def __init__(self, url, council_id, table, extension=None):
         self.url = url
         self.council_id = council_id
         self.table = table
+        self.extension = extension
         super().__init__()
 
     def make_geometry(self, feature):
@@ -17,4 +19,12 @@ class HashOnlyScraper(BaseScraper):
 
     def scrape(self):
         data = get_data_from_url(self.url)
+
+        if self.extension:
+            sync_file_to_github(
+                self.council_id,
+                "%s.%s" % (self.table, self.extension),
+                data
+            )
+
         self.store_history(data, self.council_id)
