c7db10939ec383282b9af30397668bb38877b020
chris48s <chris.shaw480@gmail.com>
2017-11-16 20:21:54 +0000

    allow SCRAPERWIKI_DATABASE_NAME to be overridden
    e.g: in a unit test

--- dc_base_scrapers/__init__.py
+++ dc_base_scrapers/__init__.py
@@ -1,4 +1,6 @@
 import os
 # hack to override sqlite database filename
 # see: https://help.morph.io/t/using-python-3-with-morph-scraperwiki-fork/148
-os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
+
+if not 'SCRAPERWIKI_DATABASE_NAME' in os.environ:
+    os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
