5288a1ba7bcaf26170fef01af3ea60019c2ea068
chris48s <chris.shaw480@gmail.com>
2018-10-20 12:26:03 +0100

    re-order packages

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     url="https://github.com/wdiv-scrapers/dc-base-scrapers/",
     packages=['dc_base_scrapers'],
     install_requires=[
+        'arcgis2geojson==1.3.0',
+        'commitment==2.0.0',
         'lxml==3.6.4',
         'retry==0.9.2',
         'scraperwiki==0.5.1',
-        'arcgis2geojson==1.3.0',
-        'commitment==2.0.0',
     ],
 )
