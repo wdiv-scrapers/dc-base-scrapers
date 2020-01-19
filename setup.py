9a579432b616ffa7d6b0caeea4d7f1cbba5f2a0c
chris48s <chris.shaw480@gmail.com>
2020-01-19 13:50:00 +0000

    bump arcgis2geojson

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/wdiv-scrapers/dc-base-scrapers/",
     packages=['dc_base_scrapers'],
     install_requires=[
-        'arcgis2geojson>=1.3,<2',
+        'arcgis2geojson>=2,<3',
         'commitment>=2,<3',
         'geojson-rewind==0.2.0',
         'lxml>=4.2,<5',
